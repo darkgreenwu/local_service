e1b0231b30eb4c245d7958ca8c9b9e5205458ea2
wusicheng <312189411@qq.com>
2016-12-16 16:18:36 +0800

    修改bug

--- templates/weather/weather.js
+++ templates/weather/weather.js
@@ -3,7 +3,7 @@ angular.module("myapp").controller("weather",function($scope,$http,$ionicLoading
     //天气api
         var url="https://wthrcdn.etouch.cn/weather_mini?city=重庆";
     //显示加载图标
-    $ionicLoading.show();
+    $ionicLoading.show()
     //请求数据
     $http.get(url).success(function(data){
         $ionicLoading.hide();
